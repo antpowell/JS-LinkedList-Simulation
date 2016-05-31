3d4d8560506f4f47f4873131a431df9d08a68493
antpowell <powell.anthony.e@gmail.com>
2016-05-31 15:14:40 -0500

    clean up and add engine to get heroku web app working

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
-var port = Number(proccess.end.PORT||3000);
+//var port = Number(proccess.end.PORT||3000);
 
-app.listen(port);
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname));
+
+app.listen(app.get('port'), function(){
+    console.log('Node app is running on port', app.get('port'));
+});
